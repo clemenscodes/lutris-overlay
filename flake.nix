7f7a8b276663c968480fff0afcf7e778ccadb000
Clemens Horn <clemens.horn@mni.thm.de>
2025-03-23 09:39:56 +0100

    feat(wine): add wine-wow-staging-10_4 to closure

--- flake.nix
+++ flake.nix
@@ -32,6 +32,7 @@
     };
     unwrapped-lutris = pkgs.lutris.override {
       extraPkgs = pkgs: [
+        inputs.wine-overlays.packages.${system}.wine-wow-staging-10_4
         inputs.wine-overlays.packages.${system}.wine-wow64-staging-10_4
         inputs.wine-overlays.packages.${system}.wine-wow64-staging-winetricks-10_4
         pkgs.winetricks
