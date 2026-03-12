cfdd26e38f176b5c4ef45e7be8e7b1bcded22d6a
Clemens Horn <me@clemenshorn.com>
2026-03-12 20:23:23 +0100

    update lutris

--- flake.nix
+++ flake.nix
@@ -3,12 +3,15 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
+    wine-overlays = {
+      url = "github:clemenscodes/wine-overlays";
+    };
   };
   outputs = {
     self,
     nixpkgs,
     ...
-  }: let
+  } @ inputs: let
     system = "x86_64-linux";
     inherit (pkgs) lib;
     pkgs = import nixpkgs {
@@ -22,66 +25,69 @@
             "steam-unwrapped"
           ];
       };
+      overlays = [
+        (final: prev: let
+          inherit (inputs.wine-overlays.packages.${system}) wine-11_4 winetricks-compat-11_4;
+        in {
+          wine = wine-11_4;
+          winetricks-compat = winetricks-compat-11_4;
+        })
+      ];
     };
     overlays = import ./overlays {inherit self system;};
-    unwrapped-lutris = pkgs.lutris.override {
-      extraPkgs = pkgs: [
-        pkgs.winetricks
-        pkgs.gamescope
-        pkgs.mangohud
-        pkgs.gamemode
-        pkgs.wget
-        pkgs.curl
-        pkgs.zenity
-        pkgs.samba
-        pkgs.jansson
-        pkgs.gnutls
-        pkgs.python3
-        pkgs.python313Packages.protobuf
-        pkgs.protobuf
-        pkgs.mesa
-        pkgs.driversi686Linux.mesa
-        pkgs.wineWow64Packages.stagingFull
-        (
-          pkgs.stdenv.mkDerivation {
-            name = "winetricks-fix";
-            phases = "installPhase";
-            installPhase = ''
-              mkdir -p $out/bin
-              ln -s ${pkgs.wineWow64Packages.stagingFull}/bin/wine $out/bin/wine64
-            '';
-          }
-        )
-      ];
-      extraLibraries = pkgs: [
-        pkgs.samba
-        pkgs.jansson
-        pkgs.gnutls
-        pkgs.libadwaita
-        pkgs.gtk4
-        pkgs.python3
-        pkgs.python313Packages.protobuf
-        pkgs.protobuf
-        pkgs.mesa
-        pkgs.driversi686Linux.mesa
-      ];
-      steamSupport = true;
-    };
-    wrapped-lutris = pkgs.writeShellApplication {
-      name = "wrapped-lutris";
-      runtimeInputs = [unwrapped-lutris];
-      text = ''
-        export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION="python"
-        exec lutris "$@"
-      '';
-    };
-    lutris = pkgs.stdenv.mkDerivation {
+    unwrapped-lutris =
+      (pkgs.lutris.override {
+        lutris-unwrapped = pkgs.lutris-unwrapped.overrideAttrs (_: rec {
+          version = "0.5.22";
+          src = pkgs.fetchFromGitHub {
+            owner = "lutris";
+            repo = "lutris";
+            tag = "v${version}";
+            hash = "sha256-4mNknvfJQJEPZjQoNdKLQcW4CI93D6BUDPj8LtD940A=";
+          };
+        });
+      }).override {
+        extraPkgs = pkgs: [
+          pkgs.winetricks
+          pkgs.gamescope
+          pkgs.mangohud
+          pkgs.gamemode
+          pkgs.wget
+          pkgs.curl
+          pkgs.zenity
+          pkgs.samba
+          pkgs.jansson
+          pkgs.gnutls
+          pkgs.python3
+          pkgs.python313Packages.protobuf
+          pkgs.protobuf
+          pkgs.mesa
+          pkgs.driversi686Linux.mesa
+          pkgs.vulkan-tools
+          pkgs.wine
+          pkgs.winetricks-compat
+        ];
+        extraLibraries = pkgs: [
+          pkgs.samba
+          pkgs.jansson
+          pkgs.gnutls
+          pkgs.libadwaita
+          pkgs.gtk4
+          pkgs.python3
+          pkgs.python313Packages.protobuf
+          pkgs.protobuf
+          pkgs.mesa
+          pkgs.driversi686Linux.mesa
+        ];
+        steamSupport = true;
+      };
+    lutris = pkgs.symlinkJoin {
       name = "lutris";
-      phases = "installPhase";
-      installPhase = ''
-        mkdir -p $out/{bin,share}
-        ln -s ${unwrapped-lutris}/share/* $out/share
-        ln -s ${wrapped-lutris}/bin/wrapped-lutris $out/bin/lutris
+      paths = [unwrapped-lutris];
+      nativeBuildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/lutris \
+          --set PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION "python"
       '';
     };
   in {
@@ -97,17 +103,8 @@
         default = pkgs.mkShell {
           buildInputs = [
             self.packages.${system}.lutris
-            pkgs.wineWow64Packages.stagingFull
-            (
-              pkgs.stdenv.mkDerivation {
-                name = "winetricks-fix";
-                phases = "installPhase";
-                installPhase = ''
-                  mkdir -p $out/bin
-                  ln -s ${pkgs.wineWow64Packages.stagingFull}/bin/wine $out/bin/wine64
-                '';
-              }
-            )
+            pkgs.wine
+            pkgs.winetricks-compat
           ];
           shellHook = ''
             echo "Lutris configured..."
